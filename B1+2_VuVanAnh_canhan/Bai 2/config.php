380bf3e01e6769a39fa91b116475751e1892a349
Vananh02 <99793514+Vananh02@users.noreply.github.com>
2023-03-27 01:41:15 +0700

    Bai 2

---
+++ B1+2_VuVanAnh_canhan/Bai 2/config.php
@@ -0,0 +1,14 @@
+
+<?php
+$db_host = 'localhost';
+$db_name = 'users';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
